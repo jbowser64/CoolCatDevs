8112e6fa664e057e7595af3960e24b6f2729c69b
Ethan-Purdue <ethanbranham@student.purdueglobal.edu>
2024-10-13 21:28:50 -0500

    Moved faveicon.ico to static

--- CoolCatDevs/Cool Cat Development/app.py
+++ CoolCatDevs/Cool Cat Development/app.py
@@ -3,17 +3,11 @@
 from flask import Flask, send_from_directory, session, request, Response, redirect, render_template, jsonify
 from flask_session import Session
 from Util import Database
-from pprint import pprint
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-#-------------------------[ Images ]-------------------------#
-@app.route("/favicon.ico", methods=["GET"])
-def favicon():
-	return send_from_directory(app.root_path, "favicon.ico")
 
 #-------------------------[ Pages ]------------------------- #
 @app.route("/")
@@ -150,7 +144,3 @@ def order_details(order_id):
 #--------------------------[ Run ]-------------------------- #
 if __name__ == "__main__":
 	app.run(debug=False)
-	#pprint(
-	#	Database.get_orders(74)
-	#)
-	#	tammyjones@example.net
